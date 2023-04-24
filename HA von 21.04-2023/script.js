d59f14f4d0d35e0c133bd4c59400720966bdfbb7
OlgaDi <129493572+olgamgsd2603@users.noreply.github.com>
2023-04-24 15:05:11 +0200

    Add files via upload

---
+++ HA von 21.04-2023/script.js
@@ -0,0 +1,32 @@
+// const number = prompt("назови число")
+// const proz = number * 90 / 100
+// console.log(proz)
+
+
+// const number1 = +prompt("назови число")
+// const number2 = +prompt("назови второе число")
+// if(number1 > number2) {
+//     console.log(number2)
+// } 
+//     else if(number1 < number2) {
+//          console.log(number1)
+//         }
+
+// const number1 = +prompt("назови первое числo")
+// if(number1 > 100) {
+//         console.log("число больше 100")
+// } else if(number1 == 100) {
+//        console.log("равно 100")
+//    } else{
+//         console.log("число меньше 100")
+//     }
+
+const userName = prompt("как вас зовут")
+const userAge = prompt("wie alt du bist")
+if(userAge > 18) {
+             console.log("hello, " + userName)
+ } else if(userAge == 18) {
+        console.log("hello, " + userName)
+    } else{
+         console.log("hi, " + userName)
+     }
